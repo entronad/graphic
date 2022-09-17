b2dca9dd3794b2f8f731827deb6ae4567a4c6153
xncz8h <xncz8h@gmail.com>
2022-09-17 10:52:33 +0200

    Add our debug page to home

--- issue_example/lib/main.dart
+++ issue_example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'myGraph.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LineAreaPointPage(),
     );
   }
 }
